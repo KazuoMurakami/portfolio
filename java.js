dab50f1e993443e6ce84a3339b69cabec10740d3
KazuoMurakami <otaviokazuo12@hotmail.com>
2022-11-15 02:32:11 -0300

    alteração

--- java.js
+++ java.js
@@ -1,5 +1,8 @@
 
-
 document.querySelector('.icone').addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
+    document.querySelector(".container").classList.toggle("show-menu"); //toggle funciona como um interruptor,acender e apagar.
+})
+
+document.querySelector('.button').addEventListener("click", function(){
+    document.querySelector(".container").classList.add("show-menu"); // add adiciona permanentemente essa clase até que ocorra uma remoção
 })
