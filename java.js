b62fc4a8ec7b3cf97957bdbebb314ffe88a44551
Kazuo <otaviokazuo12@hotmail.com>
2023-01-15 22:45:29 -0300

    Nova alteração

--- java.js
+++ java.js
@@ -1,8 +1,12 @@
-
+// Anexar um evento de clique ao elemento com a classe 'icone'
 document.querySelector('.icone').addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu"); //toggle funciona como um interruptor,acender e apagar.
-})
-
-document.querySelector('.button').addEventListener("click", function(){
-    document.querySelector(".container").classList.add("show-menu"); // add adiciona permanentemente essa clase até que ocorra uma remoção
-})
+    // Quando o elemento é clicado, alternar a classe 'show-menu' no elemento com a classe 'container'
+    document.querySelector(".container").classList.toggle("show-menu");
+  });
+  
+  // Anexar um evento de clique ao elemento com a classe 'button'
+  document.querySelector('.button').addEventListener("click", function(){
+    // Quando o elemento é clicado, adicionar a classe 'show-menu' ao elemento com a classe 'container'
+    document.querySelector(".container").classList.add("show-menu");
+  });
+  
